073a7e503249ad5b7a189210f9b65c574ee9c6a0
Ömer Eryılmaz <omreryilmaz@gmail.com>
2015-06-04 20:16:23 +0300

    Algoritma Analizi Sınav

---
+++ Soru6_EnFazlaSifirliAltDizi/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnFazlaSifirliAltdizi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = new int[] { 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,1,1};
+            enFazlaSifirliAltDiziBul(dizi);
+        }
+
+        private static void enFazlaSifirliAltDiziBul(int[] dizi)
+        {
+            int[] sifirliAltDiziler =new int[dizi.Length];
+            int sayici = 0, sayi = 0;
+            Console.Write("Dizi elemanlari :");
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (dizi[i] == 0)
+                {
+                    sayici++;
+                }
+                else if(dizi[i]==1 && sayici>0)
+                {
+                    sifirliAltDiziler[sayi] = sayici;
+                    sayici = 0;
+                    sayi++;
+                }
+                Console.Write(" "+dizi[i]+" ");
+            }
+
+            sifirSayisi(sifirliAltDiziler);
+        }
+
+        private static void sifirSayisi(int[] sifirliAltDiziler)
+        {
+            int enb = 0;
+            for (int i = 0; i < sifirliAltDiziler.Length; i++)
+            {
+                if (sifirliAltDiziler[i] > enb)
+                    enb = sifirliAltDiziler[i];
+            }
+            Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine("En cok sifira sahip alt dizinin sifir sayisi : " + enb);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Soru8_DinamikDiziResize/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soru8_DinamikDiziResize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> m = new MyStack<int>();
+            int sayi = 0;
+            Console.Write("Kac Sayi Gireceksin? :");
+            sayi =Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sayi; i++)
+            {
+                Console.Write("Sayi Gir :");
+                m.push(Int32.Parse(Console.ReadLine()));
+                Console.WriteLine("");
+            }
+
+            elemanlariTerstenYazdir(m,sayi);
+            Console.ReadKey();
+        }
+
+        private static void elemanlariTerstenYazdir(MyStack<int> m, int sayi)
+        {
+            Console.Write("Girilen Sayilar : ");
+            for (int i = 0; i <sayi; i++)
+            {
+                Console.Write(m.pop() +" ");
+            }
+        }
+    }
+
+    class MyStack<T>
+    {
+        private T[] veri { get; set; }
+        private int boyut;
+        private int kapasite;
+
+        public MyStack()
+        {
+            boyut = 0;
+            kapasite = 3;
+            veri = new T[kapasite];
+            Console.WriteLine("Boyut : "+boyut+" Kapasite : "+kapasite);
+        }
+        internal void push(T i)
+        {
+            if (boyut >= kapasite) resize();
+            veri[boyut++] = i;
+            Console.WriteLine("PUSH Boyut : " + boyut + " Kapasite : " + kapasite);
+		}
+
+
+        private void resize()
+        {
+            kapasite *= 2;
+            T[] yeniveri = new T[kapasite];
+            for (int i = 0; i < veri.Length; i++)
+            {
+                yeniveri[i] = veri[i];
+            }
+            veri = yeniveri;
+            Console.WriteLine("Kapasite Arttirildi.");
+            Console.WriteLine("RESIZE Boyut : " + boyut + " Kapasite : " + kapasite);
+        }
+
+        public T pop()
+        {           
+            return veri[--boyut];
+        }
+    }
+}
